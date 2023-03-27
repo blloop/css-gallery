f50878259c4cbaab4b2c54b185f87f90a1a5ac22
blloop <42186044+blloop@users.noreply.github.com>
2023-03-26 22:59:49 -0700

    add basic content structure

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,25 @@
 'use client'
 
+import { useState } from 'react'
+
 export default function Page() {
+  const [paintings, setPaintings] = useState([{name: 'New Painting', text: ''}])
+  
   return (
     <div>
       <p> Gallery of CSS Components </p>
       <a href="/"> Back to home </a>
+      {paintings.map((p) => 
+        <div className='painting'>
+          <p> Name: </p>
+          <input defaultValue={p.name}/>
+          <p> Text: </p>
+          <input defaultValue={p.text}/>
+        </div>
+      )}
+      <button onClick={() => setPaintings(
+        [...paintings, {name: 'New Painting', text: ''}]
+      )}> Add New </button>
     </div>
   )
 }
