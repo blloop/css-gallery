e27679684b5ea981275105abef6998722db70671
blloop <42186044+blloop@users.noreply.github.com>
2023-04-29 23:04:08 -0700

    add copy to clipboard

--- src/app/gallery/painting.tsx
+++ src/app/gallery/painting.tsx
@@ -1,17 +1,24 @@
 'use client'
 
 const Painting = (props: any) => {
+
+  let copyClipboard = () => {
+    navigator.clipboard.writeText(props.text);
+  }
   return (    
     <div className='painting' key={props.id}>
       {/* <p> Name: {props.title} </p> */}
       <p> Add your code below! </p>
+      <button className='copy-button' 
+        onClick={event => copyClipboard()}> 
+        Copy to clipboard 
+      </button>
       <textarea className='paint'
         onChange={(event) => {
           props.setPaintNum(props.id, event.target.value)
         }}
         value={props.text}/>
       <div dangerouslySetInnerHTML={{ __html: props.text }} />
-      {/* TODO: Add copy to clipboard button */}
     </div>
   )
 }
