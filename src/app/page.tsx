a3d396085c35bb82a0f2b14ec3a2de4f6d441cd8
blloop <42186044+blloop@users.noreply.github.com>
2023-05-07 22:56:43 -0700

    update page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import './page.css'
+
 import NavBar from './navbar'
 
 export default function Home() {
   return ( <>
     <NavBar page={0}/>
-    <div>
-      <p> This is the home page </p>
-    </div>
+      <div className='main-text'>
+        <p className='subtitle'> This is a simple CSS gallery </p>
+        <p> Here, you can view various examples of inline CSS </p>
+      </div>
   </> )
 }
