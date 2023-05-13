bd4e7cfaae8044b31eea39fa2f349214ee8682e2
blloop <42186044+blloop@users.noreply.github.com>
2023-05-12 22:26:39 -0700

    basic guide content

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,12 +1,23 @@
 'use client'
 
+import './page.css'
+
 import NavBar from '../navbar'
 
 export default function Home() {
   return ( <>
     <NavBar page={2}/>
-    <div>
-      <p> How to use this app </p>
-    </div>
+      <div className='main-text'>
+        <p className='subtitle'> How to use this app </p>
+        <ul>
+          <li> 
+            Modify the contents of a "painting" by changing the
+            corresponding text
+          </li>
+          <li>
+            Add a new "painting" by pressing the "+" button
+          </li>
+        </ul>
+      </div>
   </> )
 }
